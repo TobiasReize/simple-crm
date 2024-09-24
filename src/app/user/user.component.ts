b3ddd9b70ea174c792591c11deb51a86a1602855
Tobias Reize <reizeto@web.de>
2024-09-24 16:18:44 +0200

    added email and user-detail-component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,23 +4,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
 import { FirebaseService } from '../firebase-service/firebase.service';
 import { DocumentData, onSnapshot, Unsubscribe } from '@angular/fire/firestore';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent implements OnInit, OnDestroy {
 
   readonly dialog = inject(MatDialog);
-  user = new User();
   allUsers: DocumentData[] = [];
   unsubUsersCol!: Unsubscribe;
 
@@ -29,12 +28,27 @@ export class UserComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     let usersColRef = this.firebaseService.getUsersColRef();
-    this.unsubUsersCol = onSnapshot(usersColRef, changes => {
+    this.unsubUsersCol = onSnapshot(usersColRef, usersCollection => {
       this.allUsers = [];
-      changes.forEach(user => {
-        this.allUsers.push(user.data());
+      usersCollection.forEach(user => {
+        this.allUsers.push(this.setUserObject(user.data(), user.id));
       });
+      console.log('All users (inkl. id):', this.allUsers);
     });
+  }
+
+
+  setUserObject(obj: any, id: string) {
+    return {
+      firstName: obj.firstName,
+      lastName: obj.lastName,
+      birthDate: obj.birthDate,
+      street: obj.street,
+      zipCode: obj.zipCode,
+      city: obj.city,
+      email: obj.email,
+      id: id
+    }
   }
 
 
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -5,6 +5,7 @@ export class User {
     street: string;
     zipCode: number;
     city: string;
+    email: string;
 
 
     constructor(obj?: any) {
@@ -14,7 +15,9 @@ export class User {
         this.street = obj ? obj.street : '';
         this.zipCode = obj ? obj.zipCode : '';
         this.city = obj ? obj.city : '';
+        this.email = obj ? obj.email : '';
     }
+
 
     public toJSON() {
         return {
@@ -23,7 +26,8 @@ export class User {
             birthDate: this.birthDate,
             street: this.street,
             zipCode: this.zipCode,
-            city: this.city
+            city: this.city,
+            email: this.email,
         }
     }
 }
